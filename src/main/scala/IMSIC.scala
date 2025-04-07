c86c7d4de9c26934beb75e2ad1b57e1e2d652599
upsetgrass <18328591636@163.com>
2025-04-07 09:55:37 +0800

    change illegal to ignore

--- src/main/scala/IMSIC.scala
+++ src/main/scala/IMSIC.scala
@@ -238,7 +238,9 @@ class IMSIC(
         ) ++ (0 until eips.length).map(i => 0x82 + i * 2
         ).toSeq ++ (0 until eies.length).map(i => 0xc2 + i * 2
         ).toSeq ++ (0 until 16).map(i => 0x30 + i).toSeq
+      val ignoredAddresses = Seq(0x71) ++ (0x73 to 0x7F)
       val isValidAddress = VecInit(validAddresses.map(_.U === fromCSR.addr.bits)).asUInt.orR
+      val isIgnoredAddress = VecInit(ignoredAddresses.map(_.U === fromCSR.addr.bits)).asUInt.orR
       def bit0ReadOnlyZero(x: UInt): UInt = x & ~1.U(x.getWidth.W)
       def fixEIDelivery(x: UInt): UInt = x & 1.U
       RegMapDV.generate(
@@ -264,7 +266,7 @@ class IMSIC(
       )
       val illegal_csr = WireDefault(false.B)
       when(fromCSR.addr.bits >= 0x00.U && fromCSR.addr.bits <= 0xFF.U &&
-          !isValidAddress) {
+          !isValidAddress && !isIgnoredAddress) {
         illegal_csr := true.B
       }
       toCSR.illegal := (fromCSR.addr.valid | fromCSR.wdata.valid) & (
