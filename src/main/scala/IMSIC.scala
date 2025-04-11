589ea6ab1778ea0bc40717912855af60d38d1c86
upsetgrass <18328591636@163.com>
2025-04-11 09:15:18 +0800

    Revert "debug:illegal ignored"
    This reverts commit 031736e21c8e4df3b18ef2ab9df2f09b6f80d065.

--- src/main/scala/IMSIC.scala
+++ src/main/scala/IMSIC.scala
@@ -116,7 +116,7 @@ case class IMSICParams(
     // MC 👉 本IMSIC的监管态和客户态中断文件的地址（Addr for supervisor-level and guest-level interrupt files for this IMSIC）:
     sgAddr: Long = 0x10000L,
     // MC 👉 客户中断文件的数量（Number of guest interrupt files）:
-    geilen: Int = 7,
+    geilen: Int = 5,
     // MC vgein信号的位宽（The width of the vgein signal）:
     vgeinWidth: Int = 6,
     // MC iselect信号的位宽(The width of iselect signal):
@@ -238,11 +238,8 @@ class IMSIC(
         ) ++ (0 until eips.length).map(i => 0x82 + i * 2
         ).toSeq ++ (0 until eies.length).map(i => 0xc2 + i * 2
         ).toSeq ++ (0 until 16).map(i => 0x30 + i).toSeq
-      val ignoredAddresses = Seq(0x71) ++ (0x73 to 0x7F)
-      val isValidAddress = validAddresses.map(a => fromCSR.addr.bits === a.U).reduce(_ || _)
-      val isIgnoredAddress = ignoredAddresses.map(a => fromCSR.addr.bits === a.U).reduce(_ || _)
+      val isValidAddress = VecInit(validAddresses.map(_.U === fromCSR.addr.bits)).asUInt.orR
       def bit0ReadOnlyZero(x: UInt): UInt = x & ~1.U(x.getWidth.W)
-      
       def fixEIDelivery(x: UInt): UInt = x & 1.U
       RegMapDV.generate(
         0.U,
@@ -267,7 +264,7 @@ class IMSIC(
       )
       val illegal_csr = WireDefault(false.B)
       when(fromCSR.addr.bits >= 0x00.U && fromCSR.addr.bits <= 0xFF.U &&
-          !(isValidAddress || isIgnoredAddress)) {
+          !isValidAddress) {
         illegal_csr := true.B
       }
       toCSR.illegal := (fromCSR.addr.valid | fromCSR.wdata.valid) & (
